79717c7fc0425f893c59e1d26d07376917261edb
Quang Dang <quangdangfit@gmail.com>
2023-01-10 10:41:48 +0700

    event store db append stream

--- eventstore/main.go
+++ eventstore/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"time"
 
 	"github.com/EventStore/EventStore-Client-Go/esdb"
 	"github.com/gofrs/uuid"
@@ -22,25 +23,37 @@ func main() {
 
 	defer db.Close() // nolint: errcheck
 
-	expectedRevision := esdb.NoStream{}
-	eventId, _ := uuid.NewV4()
-	data, _ := json.Marshal("data")
-	eventsData := []esdb.EventData{
-		{
-			EventID:   eventId,
-			EventType: "order",
-			Data:      data,
-		},
-	}
-	appendStream, err := db.AppendToStream(
-		context.Background(),
-		"streamId",
-		esdb.AppendToStreamOptions{ExpectedRevision: expectedRevision},
-		eventsData...,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		expectedRevision := esdb.StreamExists{}
+		eventId, _ := uuid.NewV4()
+		data := map[string]interface{}{
+			"id":             "63283f04f84bcc8e1893b09c",
+			"created_at":     1663581956789,
+			"created_by":     "62efee8d1bbe176aa8feeab9",
+			"partnership_id": "630720682f9a32ef3356b3ab",
+			"ticket_id":      "6321521b1f9c7f6e8665c437",
+			"time":           1663581956789,
+		}
+		bData, _ := json.Marshal(data)
+		eventsData := []esdb.EventData{
+			{
+				EventID:     eventId,
+				EventType:   "order",
+				ContentType: esdb.JsonContentType,
+				Data:        bData,
+			},
+		}
+		appendStream, err := db.AppendToStream(
+			context.Background(),
+			"$checkin-logs-stream-1",
+			esdb.AppendToStreamOptions{ExpectedRevision: expectedRevision},
+			eventsData...,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	log.Printf("(Save) stream: {%+v}", appendStream)
+		log.Printf("(Save) stream: {%+v}", appendStream)
+		time.Sleep(time.Second)
+	}
 }
